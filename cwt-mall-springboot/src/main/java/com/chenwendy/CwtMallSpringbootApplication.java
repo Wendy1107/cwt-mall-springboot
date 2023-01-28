8ef07b6bc310bc916c9c69c91cf7c9d4cdba5a1f
LAPTOP-CLI1VKRV\wen82 <wen821107@gmail.com>
2023-01-28 15:15:53 +0800

    初始化 Spring Boot project

---
+++ cwt-mall-springboot/src/main/java/com/chenwendy/CwtMallSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.chenwendy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwtMallSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwtMallSpringbootApplication.class, args);
+	}
+
+}
---
+++ cwt-mall-springboot/src/test/java/com/chenwendy/CwtMallSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenwendy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CwtMallSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
