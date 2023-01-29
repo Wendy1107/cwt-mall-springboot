df961bf213a5d79788148e70905e75c36683a099
LAPTOP-CLI1VKRV\wen82 <wen821107@gmail.com>
2023-01-29 18:52:25 +0800

    商品功能-實作新增商品

--- cwt-mall-springboot/src/main/java/com/chenwendy/dao/ProductDao.java
+++ cwt-mall-springboot/src/main/java/com/chenwendy/dao/ProductDao.java
@@ -1,8 +1,11 @@
 package com.chenwendy.dao;
 
+import com.chenwendy.dto.ProductRequest;
 import com.chenwendy.model.Product;
 
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- cwt-mall-springboot/src/main/java/com/chenwendy/service/ProductService.java
+++ cwt-mall-springboot/src/main/java/com/chenwendy/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.chenwendy.service;
 
+import com.chenwendy.dto.ProductRequest;
 import com.chenwendy.model.Product;
 
 public interface ProductService {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
